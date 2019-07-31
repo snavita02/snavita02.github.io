c030cc3dc3dcbbc3c0900813472f57f73947c89b
snavita02 <53526068+snavita02@users.noreply.github.com>
2019-07-31 08:35:39 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,22 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I love love LOVE K-pop! I follow a lot of Kpop groups but I stan BTS! I also listen to R&B and sometimes rap.";
+}
+</script>
+</head>
+<body>
+
+<h2>Music I Like!</h2>
+
+<p id="demo">Learn more</p>
+
+<button type="button" onclick="myFunction()">Explore!</button>
+
+</body>
+</html>
